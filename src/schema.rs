1b7010811b5b9d0df210d296ebeaf48b1fa4fff9
Jose Enrique Padilla <jp06171@gmail.com>
2024-09-13 22:34:07 -0600

    user, role, user_role migrations

--- src/schema.rs
+++ src/schema.rs
@@ -17,6 +17,17 @@ diesel::table! {
 }
 
 diesel::table! {
+    role (id) {
+        id -> Int4,
+        #[max_length = 64]
+        code -> Varchar,
+        #[max_length = 128]
+        name -> Varchar,
+        created_at -> Timestamp,
+    }
+}
+
+diesel::table! {
     rustacean (id) {
         id -> Int4,
         name -> Varchar,
@@ -25,9 +36,33 @@ diesel::table! {
     }
 }
 
+diesel::table! {
+    user_role (id) {
+        id -> Int4,
+        user_id -> Int4,
+        role_id -> Int4,
+    }
+}
+
+diesel::table! {
+    users (id) {
+        id -> Int4,
+        #[max_length = 64]
+        username -> Varchar,
+        #[max_length = 128]
+        password -> Varchar,
+        created_at -> Timestamp,
+    }
+}
+
 diesel::joinable!(crate_ -> rustacean (rustacean_id));
+diesel::joinable!(user_role -> role (role_id));
+diesel::joinable!(user_role -> users (user_id));
 
 diesel::allow_tables_to_appear_in_same_query!(
     crate_,
+    role,
     rustacean,
+    user_role,
+    users,
 );
